46a45cb6fc09dfb09fed1cab6e0efc80bc12ca2a
Nains <jason20226@gmail.com>
2025-10-13 17:01:22 +0800

    test php

--- js/homeWork.js
+++ js/homeWork.js
@@ -72,18 +72,18 @@ function homeWorkFormat(data){
 }
 const homeWorkModel = new HomeWorkModel();
 
-homeWorkModel
-  .ajax(1)
-  .then((model) => model.ajax(2))
-  .then((model) => model.getData())
-  .then(homeWorkFormat)
-  .catch((err) => log(err, "w"))
-  .finally(() => log("Home Work Completed!", "s"));
-
 // homeWorkModel
-//   .backEnd(1)
-//   .then((model) => model.backEnd(2))
+//   .ajax(1)
+//   .then((model) => model.ajax(2))
 //   .then((model) => model.getData())
 //   .then(homeWorkFormat)
 //   .catch((err) => log(err, "w"))
 //   .finally(() => log("Home Work Completed!", "s"));
+
+homeWorkModel
+  .backEnd(1)
+  .then((model) => model.backEnd(2))
+  .then((model) => model.getData())
+  .then(homeWorkFormat)
+  .catch((err) => log(err, "w"))
+  .finally(() => log("Home Work Completed!", "s"));

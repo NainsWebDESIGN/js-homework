ff2ec4ee9aa6967c5555101865efb3c6dc60a17e
Nains <jason20226@gmail.com>
2025-10-13 14:22:02 +0800

    測試 CORS

--- php/postDataBase.php
+++ php/postDataBase.php
@@ -3,15 +3,16 @@
 // 设置响应头以支持 JSON
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+// header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Methods: POST");
 header('Access-Control-Allow-Credentials: true');
 header("Access-Control-Allow-Headers: Content-Type, Content-Length, Accept-Encoding, X-Requested-with, Origin, Authorization");
 header('Access-Control-Expose-Headers:X-My-Custom-Header');
 
 // 處理 OPTIONS 請求
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    exit;
-}
+// if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+//     exit;
+// }
 
 // 获取 AJAX 请求的输入数据
 $request = json_decode(file_get_contents("php://input"), true);

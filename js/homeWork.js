d11672b1e5f4d1265966a3e644da4566852fbe74
Nains <jason20226@gmail.com>
2025-10-13 12:16:49 +0800

    update database

--- js/homeWork.js
+++ js/homeWork.js
@@ -1,5 +1,17 @@
-ajaxModel
-  .ajax()
-  .then((model) => { })
+const homeWorkModel = new HomeWorkModel();
+
+// homeWorkModel
+//   .ajax(1)
+//   .then((model) => model.ajax(2))
+//   .then((model) => model.getData())
+//   .then((data) => { })
+//   .catch((err) => log(err, "w"))
+//   .finally(() => log("Home Work Completed!", "s"));
+
+homeWorkModel
+  .backEnd(1)
+  .then((model) => model.backEnd(2))
+  .then((model) => model.getData())
+  .then((data) => { })
   .catch((err) => log(err, "w"))
   .finally(() => log("Home Work Completed!", "s"));
--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
-const ajaxModel = new AjaxModel();
+const topicModel = new TopicModel();
 
-ajaxModel
-  .getTopic()
+// topicModel
+//   .getTopic()
+//   .then((res) => {
+//     const myApp = document.getElementById("myApp");
+    
+//     res.forEach((item, index) => {
+//       const newP = document.createElement("p");
+//       newP.textContent = `${index + 1}. ${item}`;
+//       myApp.appendChild(newP);
+//     });
+//   })
+//   .catch((err) => log(err, "w"))
+//   .finally(() => log("Topic Completed!", "s"));
+
+topicModel
+  .backTopic('topic')
   .then((res) => {
     const myApp = document.getElementById("myApp");
     

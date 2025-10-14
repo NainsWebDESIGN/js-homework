23bbad5ec142c2865ce68dcc8c99ce4435a71f89
Nains <jason20226@gmail.com>
2025-10-14 17:09:47 +0800

    update homework

--- php/system.php
+++ php/system.php
@@ -4,14 +4,15 @@ $method = $_SERVER['REQUEST_METHOD'];
 // 设置响应头以支持 JSON
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST");
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Headers: Content-Type, Content-Length, Accept-Encoding, X-Requested-with, Origin, Authorization');
 header('Access-Control-Expose-Headers:X-My-Custom-Header');
 
 switch ($method) {
     case 'GET':
-        header("Access-Control-Allow-Methods: GET");
         $param = $_GET['getWay'];
+        
         if($param == 'data1'){
             require "data1.php";
             echo json_encode(array('errMsg' => '', 'status' => true, 'data' => $data));
@@ -27,9 +28,8 @@ switch ($method) {
         break;
         
     case 'POST':
-        header("Access-Control-Allow-Methods: POST");
         $request = json_decode(file_get_contents("php://input"), true);
-        
+
         if($request['message'] === "topic"){
             require "topic.php";
             echo json_encode(array('errMsg' => '', 'status' => true, 'data' => $data));

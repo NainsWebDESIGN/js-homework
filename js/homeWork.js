f304fbe185b1e4c1d310a358d5333f7a6493729a
Nains <jason20226@gmail.com>
2025-10-14 11:10:41 +0800

    新增 void

--- js/homeWork.js
+++ js/homeWork.js
@@ -3,20 +3,21 @@ function homeWorkFormat(data){
 }
 const homeWorkModel = new HomeWorkModel();
 
-// 向 json 抓作業資料
-homeWorkModel
-  .ajax(1)
-  .then((model) => model.ajax(2))
+function GEThomeWork(){
+  switch(api){
+    case "json": // 向 json 抓作業資料
+      return homeWorkModel
+        .ajax(1)
+        .then((model) => model.ajax(2));
+    case "php": // 向 php 抓作業資料
+      return homeWorkModel
+        .backEnd(1)
+        .then((model) => model.backEnd(2));
+  }
+}
+
+GEThomeWork()
   .then((model) => model.getData())
   .then(homeWorkFormat)
   .catch((err) => log(err, "w"))
   .finally(() => log("Home Work Completed!", "s"));
-
-// 向 php 抓作業資料
-// homeWorkModel
-//   .backEnd(1)
-//   .then((model) => model.backEnd(2))
-//   .then((model) => model.getData())
-//   .then(homeWorkFormat)
-//   .catch((err) => log(err, "w"))
-//   .finally(() => log("Home Work Completed!", "s"));
---
+++ js/void.js
@@ -0,0 +1,56 @@
+const api = "php";
+/**
+ * 取得抓取 json 的路徑
+ * @param url (string) 檔案名稱
+ */
+function basehref(url){
+  return `./json/${url}.json`;
+}
+
+/**
+ * 取得抓取 php 的路徑
+ * @param url (string) 檔案名稱
+ */
+function backhref(url){
+  // return `https://nainsjs.zeabur.app/php/${url}.php`;
+  return `./php/${url}.php`;
+}
+
+/**
+ * 打印出有樣式的資料
+ * @param t (string) 要打印的文字(預設為正常打印)
+ * @param a (string) l = 正常，s = 成功，e = 錯誤，w = 警告
+ */
+function log(t, a = "l") {
+  const type = {
+    l: {
+      key: "log",
+      background: "#57a3f3",
+      color: "#000",
+    },
+    s: {
+      key: "success",
+      background: "#19be6b",
+      color: "#000",
+    },
+    e: {
+      key: "error",
+      background: "#ed4014",
+      color: "#000",
+    },
+    w: {
+      key: "warning",
+      background: "#ffad33",
+      color: "#000",
+    },
+  };
+
+  console.log(
+    `%c ${t} `,
+    `background: ${type[a].background}; padding: 1px; border-radius: 3px; color: ${type[a].color};`
+    // `%c ${(!type[a].key) ? a : type[a].key} %c ${t} %c`,
+    // "background: #35495E; padding: 1px; border-radius: 3px 0 0 3px; color: #fff;",
+    // `background: ${type[a].background}; padding: 1px;border-radius: 0 3px 3px 0; color: ${type[a].color}`,
+    // "background: transparant"
+  );
+}

1f821243a353bbc45300ff5183ee39dc4635b9f8
Nains <jason20226@gmail.com>
2025-10-29 13:37:22 +0800

    testZeabur

--- js/ajaxModel.js
+++ js/ajaxModel.js
@@ -23,7 +23,7 @@ class HomeWorkModel {
       let urls = [1, 2];
       const allData = await Promise.all(
         urls.map((url) =>
-          fetch(PATH.FRONT(`homeWork${url}`)).then((res) => res.json())
+          axios.get(PATH.FRONT(`homeWork${url}`)).then((res) => res.data)
         )
       );
 
@@ -49,14 +49,14 @@ class HomeWorkModel {
 
       return Promise.all(
         urls.map((url) =>
-          fetch(PATH.BACK(url), {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: encode,
-            },
-          })
-            .then((res) => res.json())
+          axios
+            .get(PATH.BACK(url), {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: encode,
+              },
+            })
+            .then((res) => res.data)
             .then((res) => {
               switch (res.status) {
                 case RESPONSE.SUC:
@@ -99,7 +99,9 @@ class TopicModel {
    */
   async getTopic() {
     try {
-      const topic = await fetch(PATH.FRONT("topic")).then((res) => res.json());
+      const topic = await axios
+        .get(PATH.FRONT("topic"))
+        .then((res) => res.data);
 
       return new Promise((res, rej) => {
         if (!topic || topic.length == 0) {
@@ -119,14 +121,14 @@ class TopicModel {
     try {
       const encode = new Jwt(payload).token;
 
-      return await fetch(PATH.BACK("topic"), {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: encode,
-        },
-      })
-        .then((res) => res.json())
+      return await axios
+        .get(PATH.HTTP("topic"), {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: encode,
+          },
+        })
+        .then((res) => res.data)
         .then((res) => {
           switch (res.status) {
             case RESPONSE.SUC:
@@ -193,15 +195,18 @@ class UserModel {
   async signup(payload) {
     try {
       const encode = new Jwt(payload).token;
-      return await fetch(PATH.BACK("signup"), {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: encode,
-        },
-        body: JSON.stringify({ message: "signup" }),
-      })
-        .then((res) => res.json())
+      return await axios
+        .post(
+          PATH.BACK("signup"),
+          { message: "signup" },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: encode,
+            },
+          }
+        )
+        .then((res) => res.data)
         .then((response) => {
           switch (response.status) {
             case RESPONSE.SUC:
